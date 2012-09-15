fee2266627a28bea4860853672c004578a39f6cd
Wendell Saligan <saliganw@gmail.com>
2012-09-15 19:27:00 +0800

    temporay changes

--- templates/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../twig/lib/Twig/Autoloader.php';
-Twig_Autoloader::register();
-$loader = new Twig_Loader_Filesystem('.');
-$twig = new Twig_Environment($loader, array('cache' => './cache'));
-echo $twig->render('test.html', array('test' => 'Hello'));
-?>
