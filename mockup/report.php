70b4eb8a1c11acc54b173799ea8d517c4b395567
Wendell Saligan <saliganw@gmail.com>
2012-09-19 16:47:16 +0800

    add zend framework

--- mockup/report.php
+++ mockup/report.php
@@ -1,11 +1,20 @@
 <?php
+header('Content-Type: application/x-pdf');
+header("Content-Disposition: inline; filename=invoice-". date("Y-m-d-H-i") . ".pdf");
+header("Cache-Control: no-cache, must-revalidate");
+
+
 $path = dirname(dirname(__FILE__)) . '/library';
 set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 require_once "Zend/Pdf.php";
 $pdf = new Zend_Pdf();
 $page = new Zend_Pdf_Page(Zend_Pdf_Page::SIZE_LETTER);
-echo "Height = {$page->getHeight()}\n";
-echo "Width = {$page->getWidth()}\n";
-$pdf->pages[0] = $page;
-$pdf->render();
+$pdf->pages[] = $page;
+$pdf->pages[] = $pdf->newPage(Zend_Pdf_Page::SIZE_LETTER);
+$font = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_TIMES_BOLD);
+$page->setFont($font,12);
+$page->drawText('wewe',110,641);
+//echo $page->getHeight();
+//echo $page->getWidth();
+//echo $pdf->render();
 ?>
