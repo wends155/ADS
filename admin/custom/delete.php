f1eb5f5716baf9e16923b582f835149f8348b197
Adam Currie <adam+bitbucket_developer@decoda.com>
2013-08-05 14:43:47 +0100

    Fix for SQL injection: https://github.com/wends155/ADS/issues/2

--- admin/custom/delete.php
+++ admin/custom/delete.php
@@ -1,9 +1,9 @@
 <?php
-	if (isset($_GET['id']))
+if ((isset($_GET['id'])) && (is_numeric($_GET['id'])))
 		{
-			include('../connect_to_mysql.php');		
-			$id = $_GET['id'];
-			mysql_query("DELETE FROM admin WHERE id='$id'");
+			include('../connect_to_mysql.php');
+			$sqlq = sprintf("DELETE FROM admin WHERE id='%d'", $_GET['id']);
+			mysql_query($sqlq);
 			header("location: view.php");
 			exit();
 		}
